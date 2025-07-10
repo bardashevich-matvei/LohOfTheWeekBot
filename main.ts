c81231f30974b663f70a7092894700cac6193a10
Matsvei Bardashevich <mbardashevich@Ascendix.net>
2025-07-10 19:24:50 +0300

    code styling

--- main.ts
+++ main.ts
@@ -6,17 +6,17 @@ dotenv.config();
 // const characterScene = new Scenes.BaseScene<Scenes.SceneContext>('characterScene');
 
 // characterScene.enter((ctx) => {
-//     ctx.reply('Введите имя персонажа:');
+//	 ctx.reply('Введите имя персонажа:');
 // });
 
 // characterScene.on('text', async (ctx) => {
-//     const characterName = ctx.message.text;
-//     ctx.reply(`Вы ввели: ${characterName}`);
+//	 const characterName = ctx.message.text;
+//	 ctx.reply(`Вы ввели: ${characterName}`);
 
-//     const info = await getCharacterInfo('silvermoon', `${characterName}`);
-//     ctx.reply(`${info}`);
+//	 const info = await getCharacterInfo('silvermoon', `${characterName}`);
+//	 ctx.reply(`${info}`);
 
-//     await ctx.scene.leave();
+//	 await ctx.scene.leave();
 // });
 
 // const stage = new Scenes.Stage<Scenes.SceneContext>([characterScene]);
@@ -29,16 +29,13 @@ bot.use(stage.middleware());
 
 bot.command('persona', (ctx) => ctx.scene.enter('characterWizard'));
 
-
 bot.on('text', async (ctx, next) => {
-    if (ctx.message.text.startsWith('/') || ctx.scene?.current) return next();
-    ctx.reply("Try /persona");
+	if (ctx.message.text.startsWith('/') || ctx.scene?.current) return next();
+	ctx.reply('Try /persona');
 });
 
 bot.launch();
 
-bot.telegram.setMyCommands([
-    { command: 'persona', description: 'WoW' },
-]);
+bot.telegram.setMyCommands([{ command: 'persona', description: 'WoW' }]);
 
 console.log('Бот запущен');
